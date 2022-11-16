b0da05656e8be1ae670a572d153af770965a94a3
vinothraj <Kanapathipillaivinothraj@gmail.com>
2022-11-16 11:58:43 +0530

    fix(mpc): fix the bugs

---
+++ 1-management-panel/client/src/Viewall.js
@@ -0,0 +1,16 @@
+// import Bootstrap-css
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import Bootstrap-js
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
+
+
+const View = () => {
+    return ( 
+        <div className="bg-info text-center text-dark">
+            <p className="fw-bold fs-2">View All</p>
+        </div>
+     );
+}
+ 
+export default View;
