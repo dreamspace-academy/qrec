92e00044f9c0da97f32b099113092aaf4be5fc93
Prajeevan <prajee.bajee@gmail.com>
2022-11-19 12:46:58 +0530

    feat: add a card to staff tab

--- 1-management-panel/client/src/pages/Staff.js
+++ 1-management-panel/client/src/pages/Staff.js
@@ -4,6 +4,16 @@ const Staff = () => {
     return (
         <div className='ui dividing header'>
             <h1>Staff Management</h1>
+            <div class="card" style={{"width":"100%"}}>
+                <img src="..." class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+            </div>
+
+
+
+
         </div>
     );
 };
