222e11cbf0b639c36ecae30fcbbea81897bfba76
Sharmiga thevarasa <soumidhev95@gmail.com>
2022-11-21 12:31:21 +0530

    feat(mpc):add form in Staff Registation

--- 1-management-panel/client/src/pages/Staffcreate.js
+++ 1-management-panel/client/src/pages/Staffcreate.js
@@ -1,21 +1,106 @@
 import React from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Form, Input, TextArea, Button, Select, Divider } from 'semantic-ui-react';
 
-const Staffcreate = () => (
+const genderOptions = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+  { key: 'o', text: 'Other', value: 'other' },
+]
+
+const Staffcreate = () => {return( 
+  <div>
+    <div className='ui dividing header'>
+      <h1>Staff Registation</h1>
+    </div>        
   <Form>
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder='First Name' />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder='Last Name' />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label='I agree to the Terms and Conditions' />
-    </Form.Field>
-    <Button type='submit'>Add</Button>
-  </Form>
-)
+    <Form.Group widths='equal'>
+    <Form.Field
+        id='form-input-control-staff-id'
+        control={Input}
+        label='Staff ID'
+        placeholder='Staff ID'
+      />
+      <Form.Field
+      id='form-input-control-error-email'
+      control={Input}
+      label='Email'
+      placeholder='joe@schmoe.com'
+      
+    />
+    </Form.Group>
+  
+    <Form.Group widths='equal'>
+      <Form.Field
+        id='form-input-control-first-name'
+        control={Input}
+        label='First name'
+        placeholder='First name'
+      />
+      <Form.Field
+        id='form-input-control-phone-number'
+        control={Input}
+        label='Phone Number'
+        placeholder='xxx xxxxxxx'
+      />
+      </Form.Group>
+      <Form.Group widths='equal'>
+      <Form.Field
+        id='form-input-control-last-name'
+        control={Input}
+        label='Last name'
+        placeholder='Last name'
+      />
+      <Form.Field
+        id='form-input-control-lab-name'
+        control={Input}
+        label='Department'
+        placeholder='Lab'
+      />
+      </Form.Group>
 
-export default Staffcreate
+      <Form.Group widths='equal'>
+      <Form.Field
+        id='form-input-control-dateofbirth-name'
+        control={Input}
+        label='D.O.B'
+        placeholder='Date of Birth'
+      />
+      <Form.Field
+        id='form-input-control-role-name'
+        control={Input}
+        label='Job Role'
+        placeholder='role'
+      />
+      </Form.Group>
+
+      <Form.Group widths='equal'>
+      <Form.Field
+        control={Select}
+        options={genderOptions}
+        label={{ children: 'Gender', htmlFor: 'form-select-control-gender' }}
+        placeholder='Gender'
+        search
+        searchInput={{ id: 'form-select-control-gender' }}
+      />
+      <Form.Field
+        id='form-input-control-remark-name'
+        control={TextArea}
+        label='Remark'
+        placeholder='text'
+
+      />
+       </Form.Group>
+    
+    
+    
+    <Form.Field
+      id='form-button-control-public'
+      control={Button}
+      content='Registor'
+      label='Confirm Details and Registor'
+    />
+  </Form>
+  </div>
+)
+    }
+export default Staffcreate;
