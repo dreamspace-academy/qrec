ea12b7e2e0693df5919cd782b0261be743ddee85
Sanjay <sanjaykirupaharan21@gmail.com>
2022-11-21 11:41:40 +0530

    feat(mpc): add sample images of staffs in staff management page

--- 1-management-panel/client/src/pages/Staff.js
+++ 1-management-panel/client/src/pages/Staff.js
@@ -21,7 +21,7 @@ const Staff = () => {
                 <div className="ui five cards">
                     <div class="ui card">
                         <div class="image">
-                            <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />
+                            <img src="https://ca.slack-edge.com/THANGN4NM-U01BUB2E1SA-afdaf31d33fa-512" />
                         </div>
                         <div class="content">
                             <div class="header">Gunarakulan</div>
@@ -34,7 +34,7 @@ const Staff = () => {
                     </div>
                     <div class="ui card">
                         <div class="image">
-                            <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />
+                            <img src="https://ca.slack-edge.com/THANGN4NM-U0490NGVDBJ-d83cdb88edee-512" />
                         </div>
                         <div class="content">
                             <div class="header">Vinothraj</div>
@@ -50,12 +50,12 @@ const Staff = () => {
                             <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />
                         </div>
                         <div class="content">
-                            <div class="header">Matthew</div>
-                            <div class="meta"><span class="date">Joined in 2015</span></div>
-                            <div class="description">Matthew is a musician living in Nashville.</div>
+                            <div class="header">Sharmiga</div>
+                            <div class="meta"><span class="date">Joined in 2022</span></div>
+                            <div class="description">Trainee</div>
                         </div>
                         <div class="extra content">
-                            <a><i aria-hidden="true" class="user icon"></i>22 Friends</a>
+                            <a>ID : 003</a>
                         </div>
                     </div>
                     <div class="ui card">
