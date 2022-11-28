6a9866925a2cbe5daba2b3a6d499f90b16d4253b
rishan <rishanrishothman@gmail.com>
2022-11-28 19:25:17 +0700

    feat:add loginpage

---
+++ 1-management-panel/client/src/pages/Loginpage.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo2.png";
+
+
+import "./Loginpage.css";
+
+function Loginpage() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <dody>
+      
+    <div className="form">
+      
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" name="login" required />
+        </div>
+      </form>
+    </div>
+    </dody>
+  );
+
+  return (
+    <div>   
+    <div className="loginpage">
+    <img src={logo}   />
+     <h1> Welcome to qRec</h1>
+     
+      <div className="login-form">
+       
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Loginpage;
