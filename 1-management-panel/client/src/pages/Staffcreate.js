e40420ba9ccc005bb4325dcdbc6d50831a8ea228
Sanjay <sanjaykirupaharan21@gmail.com>
2022-11-30 17:36:24 +0530

    feat(mpcs): add firebase database connection to staff creation

--- 1-management-panel/client/src/pages/Staffcreate.js
+++ 1-management-panel/client/src/pages/Staffcreate.js
@@ -1,110 +1,213 @@
-import React from 'react'
-import { Form, Input, TextArea, Button, Select, Divider, Image } from 'semantic-ui-react';
+import React, { useState } from 'react'
+import { Form, Input, TextArea, Button, Select, Divider, Image,  } from 'semantic-ui-react';
+import {Alert} from 'react-bootstrap';
+import StaffDataService from '../services/staffs.services';
 
 const genderOptions = [
   { key: 'm', text: 'Male', value: 'male' },
   { key: 'f', text: 'Female', value: 'female' },
 ]
 
-
-
 const Staffcreate = () => {
+  const [staff, setStaff] = useState("");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [department, setDepartment] = useState("");
+  const [dob, setDob] = useState("");
+  const [job, setJob] = useState("");
+  const [gender, setGender] = useState("");
+  const [remark, setRemark] = useState("");
+  const [message, setMessage] = useState({ error: false, msg: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    if (
+      staff === "" ||
+      fname === "" ||
+      lname === "" ||
+      email === "" ||
+      phone === "" ||
+      department === "" ||
+      dob === "" ||
+      job === "" ||
+      gender === "" ||
+      remark === ""
+    ) {
+      setMessage({ error: true, msg: "All fields are mandatory!" });
+      return;
+    }
+
+    const newStaff = {
+      staff,
+      fname,
+      lname,
+      email,
+      department,
+      job,
+      dob,
+      gender,
+      phone,
+      remark
+
+    }
+    console.log(newStaff);
+
+    try {
+      await StaffDataService.addStaffs(newStaff);
+      setMessage({ error: false, msg: "Staff added sucessfully" })
+    } catch (err) {
+      setMessage({ error: true, msg: err.message });
+    }
+
+    setStaff("");
+    setFname("");
+    setLname("");
+    setEmail("");
+    setDepartment("");
+    setJob("");
+    setDob("");
+    setGender("");
+    setPhone("");
+    setRemark("");
+
+  };
+
+
+
+
   return (
+
     <div className='container-fluid Scroll'>
       <div className='ui dividing header'>
+        
         <h1>Staff Registation</h1>
-      </div>
-      <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' control={Input} size='medium' centered rounded /> <br />
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Field
-            id='form-input-control-staff-id'
-            control={Input}
-            label='Staff ID'
-            placeholder='Staff ID'
-          />
-          <Form.Field
-            id='form-input-control-error-email'
-            control={Input}
-            label='Email'
-            placeholder='joe@schmoe.com'
+      </div>  
 
-          />
-        </Form.Group>
+      {message?.msg && (
+        <Alert
+          variant={message?.error ? "danger" : "success"}
+          dismissble
+          onClose={() => setMessage("")}
+        >
+          {" "}
+          { message?.msg}
+        </Alert>
+      )}  
 
-        <Form.Group widths='equal'>
-          <Form.Field
-            id='form-input-control-first-name'
-            control={Input}
-            label='First name'
-            placeholder='First name'
-          />
-          <Form.Field
-            id='form-input-control-phone-number'
-            control={Input}
-            label='Phone Number'
-            placeholder='xxx xxxxxxx'
-          />
-        </Form.Group>
-        <Form.Group widths='equal'>
-          <Form.Field
-            id='form-input-control-last-name'
-            control={Input}
-            label='Last name'
-            placeholder='Last name'
-          />
-          <Form.Field
-            id='form-input-control-lab-name'
-            control={Input}
-            label='Department'
-            placeholder='Lab'
-          />
-        </Form.Group>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' control={Input} size='medium' centered rounded /> <br />
 
-        <Form.Group widths='equal'>
-          <Form.Field
-            id='form-input-control-dateofbirth-name'
-            control={Input}
-            label='D.O.B'
-            placeholder='Date of Birth'
-          />
-          <Form.Field
-            id='form-input-control-role-name'
-            control={Input}
-            label='Job Role'
-            placeholder='role'
-          />
-        </Form.Group>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group widths='equal'>
+              <Form.Field
+                id='form-input-control-staff-id'
+                control={Input}
+                label='Staff ID'
+                placeholder='Staff ID'
+                value={staff}
+                onChange={(e) => setStaff(e.target.value)}
+              />
+              <Form.Field
+                id='form-input-control-error-email'
+                control={Input}
+                label='Email'
+                placeholder='joe@schmoe.com'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-        <Form.Group widths='equal'>
-          <Form.Field
-            control={Select}
-            options={genderOptions}
-            label={{ children: 'Gender', htmlFor: 'form-select-control-gender' }}
-            placeholder='Gender'
-            search
-            searchInput={{ id: 'form-select-control-gender' }}
-          />
-          <Form.Field
-            id='form-input-control-remark-name'
-            control={TextArea}
-            label='Remarks'
-            placeholder='text'
+            <Form.Group widths='equal'>
+              <Form.Field
+                id='form-input-control-first-name'
+                control={Input}
+                label='First name'
+                placeholder='First name'
+                value={fname}
+                onChange={(e) => setFname(e.target.value)}
+              />
+              <Form.Field
+                id='form-input-control-phone-number'
+                control={Input}
+                label='Phone Number'
+                placeholder='xxx xxxxxxx'
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field
+                id='form-input-control-last-name'
+                control={Input}
+                label='Last name'
+                placeholder='Last name'
+                value={lname}
+                onChange={(e) => setLname(e.target.value)}
+              />
+              <Form.Field
+                id='form-input-control-lab-name'
+                control={Input}
+                label='Department'
+                placeholder='Lab'
+                value={department}
+                onChange={(e) => setDepartment(e.target.value)}
+              />
+            </Form.Group>
 
-          />
-        </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field
+                id='form-input-control-dateofbirth-name'
+                control={Input}
+                label='D.O.B'
+                placeholder='Date of Birth'
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+              />
+              <Form.Field
+                id='form-input-control-role-name'
+                control={Input}
+                label='Job Role'
+                placeholder='role'
+                value={job}
+                onChange={(e) => setJob(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group widths='equal'>
+              <Form.Field
+                control={Input}
+                // options={genderOptions}
+                label='Gender'
+                placeholder='Gender'
+                // search
+                // searchInput={{ id: 'form-select-control-gender' }}
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              />
+              <Form.Field
+                id='form-input-control-remark-name'
+                control={TextArea}
+                label='Remarks'
+                placeholder='text'
+                value={remark}
+                onChange={(e) => setRemark(e.target.value)}
+              />
+            </Form.Group>
 
 
 
-        <Form.Field
-          id='form-button-control-public'
-          control={Button}
-          content='Register'
-          label='Confirm Details and Register'
-        />
-      </Form>
-      <br /><br /><br />
-    </div>
-  )
-}
-export default Staffcreate;
+            <Form.Field
+              id='form-button-control-public'
+              control={Button}
+              type="Submit"
+              content='Register'
+              label='Confirm Details and Register'
+            />
+          </Form>
+          <br /><br /><br />
+        </div>
+      )
+      }
+      export default Staffcreate;
