1a0b6b3c8e33b07170e4db9398bf074ac8075ad2
Rishothman <rishanrishothman@gmail.com>
2023-01-13 17:55:10 +0530

    fix: change loginpage

--- client/src/pages/Loginpage.js
+++ client/src/pages/Loginpage.js
@@ -1,104 +1,122 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import DASH from "../Components/Dash-bord";
-import logo from "../images/logo2.png"
-import "../PageStyle/Loginpage.css";
+import '../PageStyle/Loginpage.css';
 
-function Loginpage() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+import { useState } from 'react';
+import { LoginApi } from '../services/Api';
+import {storeUserData } from '../services/Storage'
+import { isAuthenticated } from '../services/Auth';
+import { Link, Navigate } from 'react-router-dom';
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
+
+export default function Loginpage (){
+
+    const initialStateErrors = {
+        email:{required:false},
+        password:{required:false},
+        custom_error:null
+    };
+    const [errors,setErrors] = useState(initialStateErrors);
+    
+    const [loading,setLoading]  =  useState(false);
+
+    const [inputs,setInputs] = useState({
+        email:"",
+        password:"",
+    })
+    const handleInput = (event)=>{
+        setInputs({...inputs,[event.target.name]:event.target.value})
     }
-  ];
 
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
+    const handleSubmit = (event)=>{
+        console.log(inputs);
+        event.preventDefault();
+        let errors =initialStateErrors; 
+        let hasError = false; 
+        
+        if (inputs.email == "") {
+            errors.email.required =true;
+            hasError=true;
+        }
+        if (inputs.password == "") {
+            errors.password.required =true;
+            hasError=true;
+        }
+       
+        if (!hasError) {
+            setLoading(true)
+            //sending login api request
+            LoginApi(inputs).then((response)=>{
+               storeUserData(response.data.idToken);
+            }).catch((err)=>{
+               if (err.code="ERR_BAD_REQUEST") {
+                    setErrors({...errors,custom_error:"Invalid Credentials."})
+               }
 
+            }).finally(()=>{
+                setLoading(false)
+            })
+        }
+        setErrors({...errors});
 
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
     }
-  };
 
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
+    if (isAuthenticated()) {
+        //redirect user to dashboard
+        return <Navigate to="/dashboard" />
+    }
 
-  // JSX code for login form
-  const renderForm = (
-    <dody>
 
-      <div className="form">
-
-        <form onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label>Username </label>
-            <input type="text" name="uname" required />
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input type="password" name="pass" required />
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-            <input type="submit" name="login" required />
-          </div>
-        </form>
+    return (
         <div>
-          Don't have an account ?
+           
+            <section className="login-block">
+                <div className="container">
+                    <div className="row ">
+                        <div className="col login-sec">
+                            <h2 className="text-center">Login Now</h2>
+                            <form onSubmit={handleSubmit} className="login-form" action="">
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1" className="text-uppercase">Email</label>
+                                <input type="email"  className="form-control" onChange={handleInput} name="email"  id="" placeholder="email"  />
+                                { errors.email.required?
+                                (<span className="text-danger" >
+                                    Email is required.
+                                </span>):null
+                                }
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1" className="text-uppercase">Password</label>
+                                <input  className="form-control" type="password" onChange={handleInput}  name="password" placeholder="password" id="" />
+                                { errors.password.required?
+                                (<span className="text-danger" >
+                                    Password is required.
+                                </span>):null
+                                }
+                            </div>
+                            <div className="form-group">
+                                {loading ?
+                                (<div  className="text-center">
+                                    <div className="spinner-border text-primary " role="status">
+                                        <span className="sr-only">Loading...</span>
+                                    </div>
+                                </div>):null
+                                }
+                                <span className="text-danger" >
+                                { errors.custom_error?
+                                (<p>{errors.custom_error}</p>)
+                                :null
+                                }
+                                </span>
+                                <input  type="submit" className="btn btn-login float-right" disabled={loading}  value="Login" />
+                            </div>
+                            <div className="clearfix"></div>
+                            <div className="form-group">
+                            Create new account ? Please <Link  to="/register">Register</Link>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </div>
-    </dody>
-  );
-
-  return (
-    <div>
-      <div className="loginpage">
-        <img src={logo} width="240px" className="logo" />
-        <h1> Welcome to qRec</h1>
-        <div className="login-form">
-          {isSubmitted ? <DASH /> : renderForm}
-        </div>
-        <br />
-
-      </div>
-    </div>
-  );
+    )
 }
-
-export default Loginpage;
